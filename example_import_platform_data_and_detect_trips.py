009c6cf41232253eb68f5060683a8917d2e767f1
Kyle <kafitz22@gmail.com>
2018-05-30 08:41:11 -0400

    Update comment

--- example_import_platform_data_and_detect_trips.py
+++ example_import_platform_data_and_detect_trips.py
@@ -10,7 +10,7 @@ itinerum = Itinerum()
 # -- Stage 1: load platform data to cache if surveys responses table does not exist
 itinerum.setup(force=False)
 
-## or perform trip detection via library algorithms
+# -- Stage 2: perform trip detection via library algorithms
 users = itinerum.load_all_users()
 detected_trips = itinerum.run_trip_detection(processing.trip_detection.triplab.algorithm, users)
 itinerum.save_trips(detected_trips)
