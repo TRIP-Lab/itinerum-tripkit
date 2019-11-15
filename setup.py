c9b45b92a8caf0d483b1bb102f60331bd9761809
K Fitzsimmons <kafitz22@gmail.com>
2019-11-15 12:58:46 -0500

    Update latest changes to new PyPI release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='itinerum-tripkit',
-    version='0.0.4',
+    version='0.0.5',
     author='Kyle Fitzsimmons',
     author_email='kafitz22@gmail.com',
     description='A toolkit for inferencing trips and trip metadata from Itinerum GPS data',
