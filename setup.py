943379854a1f24c8833984b919d21df58893865c
K Fitzsimmons <kafitz22@gmail.com>
2019-12-04 13:54:59 -0500

    Increment release version to 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='itinerum-tripkit',
-    version='0.0.12',
+    version='0.0.13',
     author='Kyle Fitzsimmons',
     author_email='kafitz22@gmail.com',
     description='A toolkit for inferencing trips and trip metadata from Itinerum GPS data',
